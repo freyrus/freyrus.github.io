26b36371ed43801c4aaa9dc00c94166a6a71346c
gialachoanglong <longnguyen.mail@gmail.com>
2014-08-20 22:40:45 +0700

    update all tool for build

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+//gọi các plugin cần thiết
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    autoprefix = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    notify = require('gulp-notify'),
+    compass = require('gulp-compass');
+
+// khai báo các đường dẫn và file sẽ dùng		
+var paths = {
+    css: [
+        'css/*.css'
+    ],
+    js: [
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'js/freyrus.js'
+    ],
+    sass: ['sass/*.scss']
+};
+
+//tạo task cho js
+gulp.task('js', function() {
+    gulp.src(paths.js)
+        .pipe(uglify())
+        .pipe(concat('freyrus.min.js'))
+        .pipe(gulp.dest('build/js'));
+});
+
+//tạo task cho css
+gulp.task('css', function() {
+    gulp.src(paths.css)
+        .pipe(concat('style.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('build/css'));
+});
+
+//tạo task cho compass
+gulp.task('compass', function() {
+    gulp.src(paths.sass)
+        .pipe(compass({
+            css: 'css',
+            sass: 'sass',
+            image: 'images'
+        }))
+        .on('error', function(err) {
+            // Would like to catch the error here
+        })
+        .pipe(gulp.dest('css'));
+});
+
+//tạo task watch để theo dõi các file nếu file thay đổi thì thực hiện các task liên quan 
+gulp.task('watch', function() {
+    gulp.watch(paths.sass, ['compass']);
+    gulp.watch(paths.css, ['css']);
+    gulp.watch(paths.js, ['js']);
+});
+
+//tạo task default : là các việc sẽ làm tuần tự khi ta chỉ dùng lệnh gulp ko kèm option task nào
+gulp.task('default', [
+    'compass',
+    'css',
+    'js',
+    'watch'
+]);
